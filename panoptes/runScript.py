2c30dc5e67be05871995ee78b02dd7ae2eaf4ee1
Kate Reeher <kreeher@four51.com>
2018-10-11 12:29:06 -0500

    still trying to work around directory issue

--- panoptes/runScript.py
+++ panoptes/runScript.py
@@ -1,14 +1,9 @@
 import pytest
-import tempfile
 
-import os
-
-path = tempfile.TemporaryDirectory().name
-print(str(path))
 
 # currently runs all tests
 
-pytest.main(['-m smoke', '--ENV=prod', '--junitxml='+str(path)+'-prod-smoke.xml'])
+pytest.main(['-m smoke', '--ENV=prod', '--junitxml="prod-smoke.xml"])
 #pytest.main(['-m', '--ENV=prod', '--junitxml='+str(path)+'-prod-search.xml'])
-pytest.main(['-m smoke', '--ENV=qa', '--junitxml='+str(path)+'-qa-smoke.xml'])
+pytest.main(['-m smoke', '--ENV=qa', '--junitxml="qa-smoke.xml"])
 #pytest.main(['-m search', '--ENV=qa', '--junitxml='+str(path)+'-qa-search.xml'])
