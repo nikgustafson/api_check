18d24c0ec89922941c3e42c39126fc5ce430d946
Kate Reeher <kreeher@four51.com>
2018-10-11 12:30:25 -0500

    still trying to work around directory issue

--- panoptes/runScript.py
+++ panoptes/runScript.py
@@ -3,7 +3,7 @@ import pytest
 
 # currently runs all tests
 
-pytest.main(['-m smoke', '--ENV=prod', '--junitxml="prod-smoke.xml"])
+pytest.main(['-m smoke', '--ENV=prod', '--junitxml="prod-smoke.xml"'])
 #pytest.main(['-m', '--ENV=prod', '--junitxml='+str(path)+'-prod-search.xml'])
-pytest.main(['-m smoke', '--ENV=qa', '--junitxml="qa-smoke.xml"])
+pytest.main(['-m smoke', '--ENV=qa', '--junitxml="qa-smoke.xml"'])
 #pytest.main(['-m search', '--ENV=qa', '--junitxml='+str(path)+'-qa-search.xml'])
