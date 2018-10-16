13845f7eeafaf5ad8ad70d41927385112acc60ea
Kate Reeher <kreeher@four51.com>
2018-10-16 08:44:17 -0500

    improving search tests, particularly around facets

--- panoptes/productFacets/__init__.py
+++ panoptes/productFacets/__init__.py
@@ -63,3 +63,93 @@ def sessionInit(configInfo):
     #log.info(json.dumps(admin.get(configInfo['API']+'v1/me').json(), indent=4))
 
     return(buyer, admin)
+
+
+def createProductFacet(configInfo, products, token):
+
+    newXP = {
+        "xp": {
+            "blue": True
+        }
+    }
+
+    log.debug(newXP)
+    newProducts = []
+    for item in products:
+        log.debug(item['ID'])
+        product = patch_Product(
+            configInfo, token, item['ID'], params='', body=newXP)
+        log.debug(product)
+        newProducts.append(product)
+
+    return newProducts
+
+
+def createProductFacet(configInfo, productList, productFacet, token=''):
+
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json',
+        'charset': 'UTF-8'
+    }
+
+    for ID in productList:
+        try:
+            xp = requests.post(configInfo['API'] + 'v1/ProductFacets/',
+                               headers=headers, params=params, json=productFacet)
+            log.debug(xp.request.url)
+            log.debug(xp.status_code)
+            log.debug(json.dumps(xp.json(), indent=2))
+
+            assert xp.status_code is codes.ok
+            return xp.json()
+        except requests.exceptions.RequestException as e:
+            log.debug(json.dumps(xp.json(), indent=2))
+            print(e)
+            sys.exit(1)
+
+
+def getProductFacets(configInfo, token, params):
+
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json',
+        'charset': 'UTF-8'
+    }
+
+    try:
+        pFacet = requests.get(
+            configInfo['API'] + 'v1/ProductFacets/', headers=headers, params=params)
+        log.debug(pFacet.request.url)
+        log.debug(pFacet.status_code)
+        log.debug(json.dumps(pFacet.json(), indent=2))
+
+        assert pFacet.status_code is codes.ok
+        return pFacet.json()
+    except requests.exceptions.RequestException as e:
+        log.debug(json.dumps(pFacet.json(), indent=2))
+        print(e)
+        sys.exit(1)
+
+
+def adminProductFacet(configInfo, token, facet):
+
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json',
+        'charset': 'UTF-8'
+    }
+
+    try:
+        pFacet = requests.post(
+            configInfo['API'] + 'v1/ProductFacets/', headers=headers, json=facet)
+        log.debug(pFacet.request.url)
+        log.debug(pFacet.status_code)
+        log.debug(json.dumps(pFacet.json(), indent=2))
+
+        assert pFacet.status_code is codes.created
+        return pFacet.json()
+    except requests.exceptions.RequestException as e:
+        log.debug(json.dumps(pFacet.json(), indent=2))
+        print(e)
+        sys.exit(1)
