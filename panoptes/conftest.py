73d8f7d88cbe04727e4a1e9be6db7a1bfea2b171
Kate Reeher <kreeher@four51.com>
2018-11-29 15:31:28 -0600

    moved setup/teardown to conftest, updated me tests

--- panoptes/conftest.py
+++ panoptes/conftest.py
@@ -12,6 +12,17 @@ from pathlib import Path
 from _pytest.runner import runtestprotocol
 from .auth import get_Token_UsernamePassword, get_anon_user_token
 import requests
+from requests import codes
+from faker import Faker
+import random
+import json
+
+
+fake = Faker()
+
+from .auth import get_Token_UsernamePassword
+from .products import get_Products, createProducts
+
 
 log = logging.getLogger(__name__)
 
@@ -136,6 +147,270 @@ def reporting_header(request):
     log.info('\n------------------------------\n| Reporting: ' +
              pytest.reporting + ' |\n------------------------------\n')
     return(pytest.reporting)
+
+
+@pytest.fixture(scope='module')
+def buyer_setup(request, configInfo, connections):
+    admin = connections['admin']
+    log.info('setting up a new buyer...')
+    buyerName = fake.company()
+
+    log.info(buyerName)
+
+    buyerSession = {
+        'clientID': '',
+        'session': '',
+        'buyerID': '',
+        'buyerName': buyerName,
+        'username': '',
+        'userID': '',
+        'password': ''
+
+    }
+
+    # make sure an incrementor exists for buyer
+    incrementors = admin.get(
+        configInfo['API'] + 'v1/incrementors')
+    # log.info(json.dumps(incrementors.json(), indent=4))
+
+    if incrementors.json()['Meta']['TotalCount'] == 0:
+        createIncrementors()
+        incrementors = admin.get(
+            configInfo['API'] + 'v1/incrementors')
+
+    else:
+        assert incrementors.json()['Meta']['TotalCount'] == 3
+
+    buyerBody = {
+        "ID": 'Buyer{buyer-incrementor}',
+        "Name": buyerName,
+        "Active": True,
+        "xp": {
+            'faker': True
+        }
+    }
+    log.debug(json.dumps(buyerBody, indent=4))
+
+    # verify that an api client exists that accepts buyers
+    filters = {
+        'AllowAnyBuyer': True,
+        'IsAnonBuyer': True
+    }
+
+    apiClients = admin.get(
+        configInfo['API'] + 'v1/apiclients', params=filters)
+    # log.info(json.dumps(apiClients.json(), indent=4))
+    assert apiClients.json()['Meta']['TotalCount'] >= 1
+    buyerSession['clientID'] = apiClients.json()['Items'][0]['ID']
+
+    # create new buyer
+    newBuyer = admin.post(
+        configInfo['API'] + 'v1/buyers', json=buyerBody)
+    log.debug(json.dumps(newBuyer.json(), indent=4))
+    assert newBuyer.status_code is codes.created
+    buyerSession['buyerID'] = newBuyer.json()['ID']
+    log.info('Created ' + buyerSession['buyerID'] + ' buyer.')
+
+    # Assign Message Sender to Buyer
+
+    messageSenders = admin.get(
+        configInfo['API'] + 'v1/messagesenders/?name=allMessageTypes')
+    log.debug(json.dumps(messageSenders.json()['Items'][0], indent=4))
+    messageID = messageSenders.json()['Items'][0]['ID']
+    assignmentBody = {
+        "MessageSenderID": messageID,
+        "BuyerID": buyerSession['buyerID']
+    }
+
+    assignMessageSender = admin.post(
+        configInfo['API'] + 'v1/messagesenders/assignments', json=assignmentBody)
+    log.debug(assignMessageSender.status_code)
+    assert assignMessageSender.status_code == 204
+    # create new user in buyer
+    userProfile = fake.profile()
+    userBody = {
+        "ID": "BuyerUser{user-incrementor}",
+        "Username": userProfile['username'],
+        "Password": fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True),
+        "FirstName": userProfile['name'].split()[0],
+        "LastName": userProfile['name'].split()[1],
+        "Email": userProfile['mail'].split('@')[0] + '.' + configInfo['MAILOSAUR-SERVER'] + '@mailosaur.io',
+        "Phone": fake.phone_number(),
+        "Active": True,
+        "xp": {
+            'faker': True
+        }
+    }
+    log.debug(json.dumps(userBody, indent=4))
+
+    buyerSession['password'] = userBody['Password']
+
+    buyerUser = admin.post(
+        configInfo['API'] + 'v1/buyers/' + buyerSession['buyerID'] + '/users', json=userBody)
+    log.debug(buyerUser.status_code)
+    assert buyerUser.status_code is codes.created
+    log.info('Created ' + buyerUser.json()['ID'] + ' user.')
+
+    buyerSession['userID'] = buyerUser.json()['ID']
+    buyerSession['username'] = buyerUser.json()['Username']
+
+    # assign security profile
+
+    securityProfiles = admin.get(
+        configInfo['API'] + 'v1/securityprofiles/buyerProfile1')
+    log.debug(json.dumps(securityProfiles.json(), indent=4))
+
+    assignBody = {
+        "SecurityProfileID": "buyerProfile1",
+        "BuyerID": buyerSession['buyerID']
+    }
+    assignProfile = admin.post(
+        configInfo['API'] + 'v1/securityprofiles/assignments', json=assignBody)
+    log.debug(assignProfile.status_code)
+    assert assignProfile.status_code is codes.no_content
+
+    # create a default ship from address
+
+    addressBody = {
+        "CompanyName": buyerName,
+        "FirstName": "",
+        "LastName": "",
+        "Street1": fake.street_address(),
+        "Street2": None,
+        "City": fake.city(),
+        "State": fake.state_abbr(),
+        "Zip": fake.zipcode(),
+        "Country": fake.country_code(),
+        "Phone": fake.phone_number(),
+        "AddressName": "DefaultShipFromAddress",
+        "xp": {
+            'fake': True
+        }
+    }
+
+    newAddress = admin.post(
+        configInfo['API'] + 'v1/addresses', json=addressBody)
+    log.debug(newAddress.status_code)
+    assert newAddress.status_code is codes.created
+
+    # create a default catalog and assign to buyer
+
+    defaultCatalog = admin.get(
+        configInfo['API'] + 'v1/catalogs/' + buyerSession['buyerID'])
+
+    assert defaultCatalog.status_code is codes.ok
+
+    # price schedule
+
+    psBody = {
+        "Name": "defaultPriceSchedule",
+        "ApplyTax": True,
+        "ApplyShipping": True,
+        "MinQuantity": 1,
+        "MaxQuantity": random.randint(0, 100),
+        "UseCumulativeQuantity": True,
+        "RestrictedQuantity": False,
+        "PriceBreaks": [
+            {
+                "Quantity": 1,
+                "Price": random.randint(0, 1000)
+            }
+        ],
+        "xp": {}
+    }
+    log.debug(psBody)
+
+    createPS = admin.post(configInfo['API'] + 'v1/priceschedules', json=psBody)
+    log.debug(createPS.status_code)
+    assert createPS.status_code is codes.created
+
+    # products create & to catalog
+    allProducts = get_Products(configInfo, admin,  {'PageSize': 100})
+    log.debug(allProducts['Meta'])
+    #assert allProducts['Meta']['TotalCount'] == 0
+
+    newProducts = createProducts(
+        configInfo, admin, DefaultPriceScheduleID=createPS.json()['ID'], numberOfProducts=20)
+    log.debug(newProducts)
+
+    allProducts = get_Products(configInfo, admin, {'PageSize': 100})
+    log.debug(allProducts['Meta'])
+
+    for item in allProducts['Items']:
+        assignProductsBody = {
+            "CatalogID": buyerSession['buyerID'],
+            "ProductID": item['ID']
+        }
+        productCatalogAssign = admin.post(
+            configInfo['API'] + 'v1/catalogs/productassignments/', json=assignProductsBody)
+        log.debug(productCatalogAssign.text)
+        log.debug(productCatalogAssign.status_code)
+        assert productCatalogAssign.status_code is codes.no_content
+
+    # set up buyer user session
+
+    log.debug(buyerSession)
+    log.debug(connections)
+
+    client_id = buyerSession['clientID']
+    username = buyerSession['username']
+    password = buyerSession['password']
+    scope = ['Shopper', 'MeAdmin', 'MeAddressAdmin']
+
+    # can successfully get a token
+    token = get_Token_UsernamePassword(
+        configInfo, client_id, username, password, scope)
+
+    buyer = requests.Session()
+
+    headers = {
+        'Authorization': 'Bearer ' + token['access_token'],
+        'Content-Type': 'application/json',
+        'charset': 'UTF-8'
+    }
+
+    buyer.headers.update(headers)
+
+    buyerSession['session'] = buyer
+
+    log.debug(buyerSession)
+
+    connections['buyer'] = buyerSession['session']
+
+    def buyer_teardown():
+        log.info('tearing down the buyer user ' +
+                 buyerUser.json()['ID'] + '...')
+
+        deleteUser = admin.delete(
+            configInfo['API'] + 'v1/buyers/' + buyerSession['buyerID'] + '/users/' + buyerUser.json()['ID'])
+        log.debug(deleteUser.status_code)
+        assert deleteUser.status_code is codes.no_content
+
+        deleteAddress = admin.delete(
+            configInfo['API'] + 'v1/addresses/' + newAddress.json()['ID'])
+        log.debug(deleteAddress.status_code)
+        assert deleteAddress.status_code is codes.no_content
+
+        log.info('tearing down the ' + buyerName + ' buyer...')
+        deleteBuyer = admin.delete(
+            configInfo['API'] + 'v1/buyers/' + buyerSession['buyerID'])
+        log.debug(deleteBuyer.status_code)
+        assert deleteBuyer.status_code is codes.no_content
+
+        log.info('tearing down the all the products...')
+        for item in allProducts['Items']:
+            deleteProduct = admin.delete(
+                configInfo['API'] + 'v1/products/' + item['ID'])
+            log.debug(deleteProduct.status_code)
+            assert deleteProduct.status_code is codes.no_content
+
+        log.info('tearing down the ' + buyerName + ' catalog...')
+        deleteCatalog = admin.delete(
+            configInfo['API'] + 'v1/catalogs/' + buyerSession['buyerID'])
+        log.debug(deleteCatalog.status_code)
+        assert deleteCatalog.status_code is codes.no_content
+
+    request.addfinalizer(buyer_teardown)
 
 
 #----------------------------# tesults set up
--- panoptes/me/test_me.py
+++ panoptes/me/test_me.py
@@ -28,8 +28,8 @@ log = logging.getLogger(__name__)
 def deleteMeAddress(configInfo, session, productID):
 
     delete = session.delete(configInfo['API'] + 'v1/me/addresses/' + productID)
-    log.info(delete.status_code)
-    # log.info(delete.json())
+    log.debug(delete.status_code)
+    # log.debug(delete.json())
 
     assert delete.status_code is codes.no_content
 
@@ -57,41 +57,24 @@ def createMeAddress(configInfo, session):
 
     create = session.post(
         configInfo['API'] + 'v1/me/addresses', json=newAddress)
-    log.info(create.json())
+    log.debug(create.json())
     assert create.status_code is codes.created
 
 
 @pytest.mark.smoke
 @pytest.mark.description(''' Verifies that a buyer user can create a new private address.\n
 						In the Smoke Tests, this verifies that the API can write to the database.''')
-def test_meAddressesCreate(buyer_setup, configInfo):
+def test_meAddressesCreate(buyer_setup, connections, configInfo):
 
-    client_id = configInfo['BUYER-CLIENTID']
-    username = configInfo['BUYER-USERNAME']
-    password = configInfo['BUYER-PASSWORD']
-    scope = ['Shopper', 'MeAdmin', 'MeAddressAdmin']
-
-    token = get_Token_UsernamePassword(
-        configInfo, client_id, username, password, scope)
-
-    buyer = requests.Session()
-
-    headers = {
-        'Authorization': 'Bearer ' + token['access_token'],
-        'Content-Type': 'application/json',
-        'charset': 'UTF-8'
-    }
-
-    buyer.headers.update(headers)
-
+    buyer = connections['buyer']
     meGet = buyer.get(configInfo['API'] + 'v1/me')
     assert meGet.status_code is codes.ok
-    # log.info(json.dumps(meGet.json(), indent=4))
+    # log.debug(json.dumps(meGet.json(), indent=4))
 
     addList = buyer.get(configInfo['API'] + 'v1/me/addresses')
     assert addList.status_code is codes.ok
 
-    # log.info(json.dumps(addList.json()))
+    # log.debug(json.dumps(addList.json()))
 
     totalAdd = addList.json()['Meta']['TotalCount']
 
@@ -110,34 +93,17 @@ def test_meAddressesCreate(buyer_setup, configInfo):
 @pytest.mark.smoke
 @pytest.mark.description(''' Verifies that a buyer user can delete a private address.\n
 						In the Smoke Tests, this verifies that the API can write deletes to the database.''')
-def test_meAddressesDelete(buyer_setup, configInfo):
-
-    client_id = configInfo['BUYER-CLIENTID']
-    username = configInfo['BUYER-USERNAME']
-    password = configInfo['BUYER-PASSWORD']
-    scope = ['Shopper', 'MeAdmin', 'MeAddressAdmin']
-
-    token = get_Token_UsernamePassword(
-        configInfo, client_id, username, password, scope)
-
-    buyer = requests.Session()
-
-    headers = {
-        'Authorization': 'Bearer ' + token['access_token'],
-        'Content-Type': 'application/json',
-        'charset': 'UTF-8'
-    }
-
-    buyer.headers.update(headers)
+def test_meAddressesDelete(buyer_setup, connections, configInfo):
+    buyer = connections['buyer']
 
     meGet = buyer.get(configInfo['API'] + 'v1/me')
     assert meGet.status_code is codes.ok
-    # log.info(json.dumps(meGet.json(), indent=4))
+    # log.debug(json.dumps(meGet.json(), indent=4))
 
     addList = buyer.get(configInfo['API'] + 'v1/me/addresses')
     assert addList.status_code is codes.ok
 
-    # log.info(json.dumps(addList.json()))
+    # log.debug(json.dumps(addList.json()))
 
     totalAdd = addList.json()['Meta']['TotalCount']
 
@@ -146,7 +112,7 @@ def test_meAddressesDelete(buyer_setup, configInfo):
         totalAdd = totalAdd + 1
 
     deletedAdd = addList.json()['Items'][0]['ID']
-    log.info('Deleted Address ID: ' + deletedAdd)
+    log.debug('Deleted Address ID: ' + deletedAdd)
 
     deleteMeAddress(configInfo, buyer, deletedAdd)
 
@@ -165,36 +131,36 @@ def test_meRegistration(configInfo, connections):
 
     # register the anon user as a new user
     newUserToken = registerMe(configInfo, anonToken)
-    log.info(json.dumps(newUserToken, indent=4))
+    log.debug(json.dumps(newUserToken, indent=4))
     jwtHeaders = jwt.get_unverified_header(newUserToken['access_token'])
 
     decoded = jwt.decode(newUserToken['access_token'], verify=False)
 
-    #log.info('America/Chicago' in common_timezones_set)
+    #log.debug('America/Chicago' in common_timezones_set)
 
     loc_tz = pytz.timezone('America/Chicago')
 
-    log.info('--------')
-    log.info('today\'s date is:')
+    log.debug('--------')
+    log.debug('today\'s date is:')
     c_dt = loc_tz.localize(datetime.today(), is_dst=False)
-    log.info(c_dt)
-    # log.info(loc_dt)
-    log.info('--------')
-    # log.info(decoded['nbf'])
-    # log.info(decoded['exp'])
+    log.debug(c_dt)
+    # log.debug(loc_dt)
+    log.debug('--------')
+    # log.debug(decoded['nbf'])
+    # log.debug(decoded['exp'])
 
-    # log.info(common_timezones_set)
+    # log.debug(common_timezones_set)
 
     notbefore = loc_tz.localize(datetime.utcfromtimestamp(
         decoded['nbf']), is_dst=False)
     expiration = loc_tz.localize(datetime.utcfromtimestamp(
         decoded['exp']), is_dst=False)
 
-    log.info('Registered User\'s NBF: ' + str(notbefore))
-    log.info('Registered User\'sEXP: ' + str(expiration))
+    log.debug('Registered User\'s NBF: ' + str(notbefore))
+    log.debug('Registered User\'sEXP: ' + str(expiration))
 
     user = get_Me(configInfo, newUserToken)
-    #log.info(json.dumps(user, indent=4))
+    #log.debug(json.dumps(user, indent=4))
 
 
 @pytest.mark.smoke
@@ -220,10 +186,10 @@ def test_me_gets(configInfo, buyer_setup, connections, sessions, endpoint):
     session = connections[sessions]
 
     collection = session.get(configInfo['API'] + 'v1/me/' + endpoint)
-    log.info(collection.url)
-    # log.info(collection.status_code)
-    log.info(repr(collection.elapsed.seconds) + ' seconds OR ' +
-             repr(collection.elapsed.microseconds) + ' microseconds')
+    log.debug(collection.url)
+    # log.debug(collection.status_code)
+    log.debug(repr(collection.elapsed.seconds) + ' seconds OR ' +
+              repr(collection.elapsed.microseconds) + ' microseconds')
     # log.debug(collection.json())
     if 'Meta' in collection.json().keys():
         log.debug(collection.json()['Meta']['TotalCount'])
@@ -232,6 +198,7 @@ def test_me_gets(configInfo, buyer_setup, connections, sessions, endpoint):
 
 
 @pytest.mark.smoke
+@pytest.mark.skip  # the buyer set up doesn't work with facets yet
 @pytest.mark.description('Verifies Facet Navigation appears on me/Products.')
 @pytest.mark.parametrize("sessions", ['buyer', 'admin'])
 def test_me_facets(configInfo, buyer_setup, connections, sessions):
@@ -239,10 +206,10 @@ def test_me_facets(configInfo, buyer_setup, connections, sessions):
     session = connections[sessions]
 
     facets = session.get(configInfo['API'] + 'v1/me/products')
-    log.info(facets.url)
-    # log.info(facets.status_code)
-    log.info(repr(facets.elapsed.seconds) + ' seconds OR ' +
-             repr(facets.elapsed.microseconds) + ' microseconds')
+    log.debug(facets.url)
+    # log.debug(facets.status_code)
+    log.debug(repr(facets.elapsed.seconds) + ' seconds OR ' +
+              repr(facets.elapsed.microseconds) + ' microseconds')
     #log.debug(json.dumps(facets.json()['Meta'], indent=4))
 
     if 'Facets' in facets.json()['Meta'].keys():
